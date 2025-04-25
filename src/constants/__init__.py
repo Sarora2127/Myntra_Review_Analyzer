bf688d8200ebbeafc820f6ab6e55967d17f7887a
Sarora2127 <saksham.arora210204@gmail.com>
2025-04-25 09:18:04 +0530

    Myntra review analyzer1

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -1,3 +1,3 @@
-MONGODB_URL_KEY:str="mongodb+srv://sakshamarora210204:NK0k7TWAZzXLlAWV@cluster0.bvcmmhr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGODB_URL_KEY:str='str'
 MONGO_DATABASE_NAME:str="myntra-reviews"
 SESSION_PRODUCT_KEY:str="product_name"
