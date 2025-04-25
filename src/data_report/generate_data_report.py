c5b8e456487d9e6bc77b56f7d2a2df13b050065f
Sarora2127 <saksham.arora210204@gmail.com>
2025-04-25 09:14:40 +0530

    Myntra review analyzer

--- src/data_report/generate_data_report.py
+++ src/data_report/generate_data_report.py
@@ -29,42 +29,6 @@ class DashboardGenerator :
         fig_bar.update_yaxes(title="Average Price")
         st.plotly_chart(fig_bar)
     
-    '''def display_product_sections(self):
-        st.header("Product Sections")
-        product_names = self.data['Product Name'].unique()
-        columns = st.columns(len(product_names))
-
-        for i, product_name in enumerate(product_names):
-            product_data = self.data[self.data['Product Name'] == product_name]
-
-            with columns[i]:
-                st.subheader(f'{product_name}')
-
-                # Display price in text or markdown with emojis
-                avg_price = product_data['Price'].mean()
-                st.markdown(f"💰 Average Price: ₹{avg_price:.2f}")
-
-                # Display average rating
-                avg_rating = product_data['Over_All_Rating'].mean()
-                st.markdown(f"⭐ Average Rating: {avg_rating:.2f}")
-
-                # Display top positive comments with great ratings
-                positive_reviews = product_data[product_data['Rating'] >= 4.5].nlargest(5, 'Rating')
-                st.subheader('Positive Reviews')
-                for index, row in positive_reviews.iterrows():
-                    st.markdown(f"✨ Rating: {row['Rating']} - {row['Comment']}")
-
-                # Display top negative comments with worst ratings
-                negative_reviews = product_data[product_data['Rating'] <= 2].nsmallest(5, 'Rating')
-                st.subheader('Negative Reviews')
-                for index, row in negative_reviews.iterrows():
-                    st.markdown(f"💢 Rating: {row['Rating']} - {row['Comment']}")
-
-                # Display rating counts in different categories
-                st.subheader('Rating Counts')
-                rating_counts = product_data['Rating'].value_counts().sort_index(ascending=False)
-                for rating, count in rating_counts.items():
-                    st.write(f"🔹 Rating {rating} count: {count}")'''
     
     def display_product_sections(self):
         st.header("🛍️ Product Showcase")
